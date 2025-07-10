6fac128786623f4eedc08b97a2ceeb72fac39fb5
Muhammad Janisar <janisarhyder90@gmail.com>
2025-07-10 15:09:55 +0500

    ok

--- 001--Basic_Types/app.js
+++ 001--Basic_Types/app.js
@@ -1,1 +1,15 @@
 "use strict";
+// primitive data types
+let a = 10;
+let b = 'janisar hyder';
+let c = false;
+// non-primitive data types (Arrays, tuples, enums)
+let x = [1, 2, 3, 4, 5, 6];
+let y = ['janisar', 1234];
+var UserRoles;
+(function (UserRoles) {
+    UserRoles["ADMIN"] = "admin";
+    UserRoles["GUEST"] = "guest";
+    UserRoles["SUPER_ADMIN"] = "super_admin";
+})(UserRoles || (UserRoles = {}));
+console.log(UserRoles.SUPER_ADMIN);
---
+++ 001--Basic_Types/app.ts
@@ -0,0 +1,18 @@
+// primitive data types
+let a = 10
+let b = 'janisar hyder'
+let c = false
+
+// non-primitive data types (Arrays, tuples, enums)
+
+let x = [1,2,3,4,5,6]
+
+let y : [string,number]= ['janisar', 1234]
+
+enum UserRoles {
+    ADMIN = 'admin',
+    GUEST = 'guest',
+    SUPER_ADMIN = 'super_admin'
+}
+console.log(UserRoles.SUPER_ADMIN);
+
